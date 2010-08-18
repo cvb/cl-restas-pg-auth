34901e75aab970de3c3ef032c2c367381dd9461e
cvb <peter@standalone.su>
2010-08-18 18:15:59 +0400

    Fix typo in packet name.

--- routes.lisp
+++ routes.lisp
@@ -13,7 +13,7 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(in-package #:cl-restas-pg-auth)
+(in-package #:restas-pg-auth)
 
 (restas:define-route login ("login")
   "")
